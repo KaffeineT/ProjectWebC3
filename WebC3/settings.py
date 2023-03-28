be344850014a853556eb3a6fc72966bfe9b31507
KaffeineT <123614327+KaffeineT@users.noreply.github.com>
2023-03-28 16:20:12 -0600

    Update settings.py

--- WebC3/settings.py
+++ WebC3/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ngtnxc%$mq_q^8yudatz4qh-@dy2_=x$m#17(^=2o67%+pdd@0'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
